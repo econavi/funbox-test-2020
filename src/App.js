0b70bf4d7876662a4fe0eef3f6abe1f601d46dd2
Vladimir Kochenov <kochenov.vl@gmail.com>
2020-01-23 01:10:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './main.scss'
+
+const App = () => {
+  return (
+    <main>
+      <section>
+        <h1>Ты сегодня покормил кота?</h1>
+      </section>
+    </main>
+  );
+}
+
+export default App
