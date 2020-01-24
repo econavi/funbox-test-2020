96405d7f6ccc54090700a6619850d52852f8ba1b
Vladimir Kochenov <kochenov.vl@gmail.com>
2020-01-24 21:19:09 +0300

    Updates

---
+++ precache-manifest.baeb8ba9561884da61d4add3666049dc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda001861f593704b42e24e6e10692ab",
+    "url": "/funbox-test-2020/index.html"
+  },
+  {
+    "revision": "3f9e8f1811946370e477",
+    "url": "/funbox-test-2020/static/css/main.26bf8b4e.chunk.css"
+  },
+  {
+    "revision": "239e07aa61c457110b87",
+    "url": "/funbox-test-2020/static/js/2.d536e37e.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/funbox-test-2020/static/js/2.d536e37e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f9e8f1811946370e477",
+    "url": "/funbox-test-2020/static/js/main.61dfcd96.chunk.js"
+  },
+  {
+    "revision": "55b6467f98edb9ac4fd2",
+    "url": "/funbox-test-2020/static/js/runtime-main.c4fb3814.js"
+  },
+  {
+    "revision": "0f494811a2bc60dbaca73be0b3f8cd11",
+    "url": "/funbox-test-2020/static/media/TrebuchetMSBold.0f494811.woff2"
+  },
+  {
+    "revision": "d67098da702832946e225fffa02ceb21",
+    "url": "/funbox-test-2020/static/media/TrebuchetMSBold.d67098da.ttf"
+  },
+  {
+    "revision": "e9e91341ef26182eac1a4e9f9471c06e",
+    "url": "/funbox-test-2020/static/media/TrebuchetMSBold.e9e91341.woff"
+  },
+  {
+    "revision": "16d279e038dfd6a61f898d1b50f7ddac",
+    "url": "/funbox-test-2020/static/media/TrebuchetMSRegular.16d279e0.woff"
+  },
+  {
+    "revision": "bf0f0f14951fa26e02b5b339edb97bef",
+    "url": "/funbox-test-2020/static/media/TrebuchetMSRegular.bf0f0f14.woff2"
+  },
+  {
+    "revision": "ede78fa18c15b24aa830f44382ef91d0",
+    "url": "/funbox-test-2020/static/media/TrebuchetMSRegular.ede78fa1.ttf"
+  },
+  {
+    "revision": "75ed7606a81262690cf3df0d95955c51",
+    "url": "/funbox-test-2020/static/media/exo2-thin.75ed7606.ttf"
+  },
+  {
+    "revision": "86ac42e0ef926b048e78543bd0550973",
+    "url": "/funbox-test-2020/static/media/exo2-thin.86ac42e0.woff2"
+  },
+  {
+    "revision": "954b8b567fe4f25b4106995463ee982b",
+    "url": "/funbox-test-2020/static/media/exo2-thin.954b8b56.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5675075705187f6db782f6481ddf01e.js"
+  "/funbox-test-2020/precache-manifest.baeb8ba9561884da61d4add3666049dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/funbox-test-2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
