7f9482656911de7d10f38169fd2f984fbbf39208
Vladimir Kochenov <kochenov.vl@gmail.com>
2020-01-24 20:00:00 +0300

    Render catalog

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import Catalog from './Components/Catalog'
+
+import mockData from './mockCatalog'
+
 import './main.scss'
+
+const makeAst = (data) => data.reduce((acc, elem) => {    
+  const newAcc = {
+    ...acc, 
+    [elem.id]: { ...elem },
+  }
+  return newAcc;
+}, {})
 
 const App = () => {
   return (
-    <main>
-      <section>
-        <h1>Ты сегодня покормил кота?</h1>
-      </section>
-    </main>
+    <section className="container">
+      <h1>Ты сегодня покормил кота?</h1>
+      <Catalog data={makeAst(mockData)} />
+    </section>
   );
 }
 
